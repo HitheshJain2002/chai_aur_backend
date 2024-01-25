c117c53fb7f4112ab9dec787b585ece3fb405194
Hithesh Jain <hitheshjain2002@gmail.com>
2024-01-25 15:43:25 +0530

    api and error handling

---
+++ utils/asyncHandler.js
@@ -0,0 +1,20 @@
+// const asyncHandler=(fn)=>
+// async(req,res,next)=>{
+// try{
+//     await fn(req,res,next)
+
+// }
+// catch(error){
+//     res.status(err.code || 500).json({
+//         sucess:false,
+//         message:err.message
+//     })
+// }
+// }
+
+const asyncHandler=(requestHandler)=>{
+(req,res,next)=>{
+    return Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+}
+}
+export {asyncHandler}
